0b2e249f5f91e9d8ebe40d64790936ac9da49e5c
Bernát Gábor <bgabor8@bloomberg.net>
2024-10-02 09:11:27 -0700

    Add uv python install
    Signed-off-by: Bernát Gábor <bgabor8@bloomberg.net>

---
+++ tasks/install_uv_python.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+from pathlib import Path
+from subprocess import check_call
+
+
+def run() -> None:
+    home = Path.home()
+    src = home / Path("Library/Application Support/uv/python")
+    dest = home / ".local" / "bin"
+    # feeds from https://github.com/indygreg/python-build-standalone/releases
+    py = {
+        "cpython": ["3.8.20", "3.9.20", "3.10.15", "3.11.10", "3.12.7", "3.13.0"],
+        "pypy": ["3.8.16", "3.9.19", "3.10.14"],
+    }
+    for impl, versions in py.items():
+        for version in versions:
+            check_call(["uv", "python", "install", f"{impl}-{version}", "--no-progress", "-r"])
+            minor = ".".join(version.split(".")[:2])
+            check_call([
+                "ln",
+                "-s",
+                "-f",
+                src / f"{impl}-{version}-macos-aarch64-none" / "bin" / "python",
+                dest / f"{"python" if impl == "cpython" else "pypy"}{minor}",
+            ])
+    check_call([
+        "ln",
+        "-s",
+        "-f",
+        src / f"cpython-{py["cpython"][-1]}-macos-aarch64-none" / "bin" / "python",
+        dest / "python",
+    ])
+
+
+if __name__ == "__main__":
+    run()
