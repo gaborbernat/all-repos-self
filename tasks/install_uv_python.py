04f0d140e37ca119e832b427a2cdcaeb6b2b6ff8
Bernát Gábor <bgabor8@bloomberg.net>
2024-11-07 09:36:23 -0800

    Add approve and auto merge
    Signed-off-by: Bernát Gábor <bgabor8@bloomberg.net>

--- tasks/install_uv_python.py
+++ tasks/install_uv_python.py
@@ -10,7 +10,7 @@ def run() -> None:
     dest = home / ".local" / "bin"
     # feeds from https://github.com/indygreg/python-build-standalone/releases
     py = {
-        "cpython": ["3.8.20", "3.9.20", "3.10.15", "3.11.10", "3.12.7", "3.13.0"],
+        "cpython": ["3.8.20", "3.9.21", "3.10.16", "3.11.11", "3.12.9", "3.13.2"],
         "pypy": ["3.8.16", "3.9.19", "3.10.14"],
     }
     for impl, versions in py.items():
@@ -22,13 +22,13 @@ def run() -> None:
                 "-s",
                 "-f",
                 src / f"{impl}-{version}-macos-aarch64-none" / "bin" / "python",
-                dest / f"{"python" if impl == "cpython" else "pypy"}{minor}",
+                dest / f"{'python' if impl == 'cpython' else 'pypy'}{minor}",
             ])
     check_call([
         "ln",
         "-s",
         "-f",
-        src / f"cpython-{py["cpython"][-1]}-macos-aarch64-none" / "bin" / "python",
+        src / f"cpython-{py['cpython'][-1]}-macos-aarch64-none" / "bin" / "python",
         dest / "python",
     ])
 
